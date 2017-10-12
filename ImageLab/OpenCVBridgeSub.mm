092aacb981b08dc3f7eb0e473d65c73f75379e53
Lucas Hansen <lchansen97@gmail.com>
2017-10-12 00:47:16 -0500

    heart rate works!

--- ImageLab/OpenCVBridgeSub.mm
+++ ImageLab/OpenCVBridgeSub.mm
@@ -1,6 +1,5 @@
 
 #import "OpenCVBridgeSub.h"
-#import "CircularBuffer.h"
 #import "PeakFinder.h"
 #import "AVFoundation/AVFoundation.h"
 
@@ -10,9 +9,9 @@ using namespace cv;
 @interface OpenCVBridgeSub()
 @property (nonatomic) cv::Mat image;
 @property (nonatomic) float* avgR;
-@property (nonatomic) float* avgG;
-@property (nonatomic) float* avgB;
 @property (nonatomic) int count;
+@property (nonatomic) char* text;
+@property (nonatomic) PeakFinder* pf;
 @end
 
 @implementation OpenCVBridgeSub
@@ -20,55 +19,125 @@ using namespace cv;
 
 -(float*) avgR{
     if(!_avgR){
-        _avgR = new float[100];
+        _avgR = new float[150];
     }
     return _avgR;
 }
--(float*) avgG{
-    if(!_avgG){
-        _avgG = new float[100];
+
+-(char*) text{
+    if(!_text){
+        _text = new char[20];
     }
-    return _avgG;
+    return _text;
 }
--(float*) avgB{
-    if(!_avgB){
-        _avgB = new float[100];
+
+-(PeakFinder*) pf{
+    if(!_pf){
+        _pf = [[PeakFinder alloc] init];
     }
-    return _avgB;
+    return _pf;
 }
 
 //@dynamic just tells the compiler that the getter and setter methods are implemented not by the class itself but somewhere else (like the superclass or will be provided at runtime).
 
+//-(void)processImage{
+//
+//    cv::Mat image_copy;
+//    char text[35];
+//    Scalar avgPixelIntensity;
+//    cv::Mat image = self.image;
+//    cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
+//    avgPixelIntensity = cv::mean( image_copy );
+//    sprintf(text,"Avg. R: %.0f, G: %.0f, B: %.0f", avgPixelIntensity.val[0],avgPixelIntensity.val[1],avgPixelIntensity.val[2]);
+//    cv::putText(image, text, cv::Point(50, 50), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+//    //Set threshhold to detect finger
+//    char text2[16] = "Finger Detected";
+//    if(avgPixelIntensity.val[0]>235 && avgPixelIntensity.val[1]<45 && avgPixelIntensity.val[2]<45){
+//        cv::putText(image, text2, cv::Point(50, 200), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+//        if (self.count < 100){
+//            self.avgR[self.count] = avgPixelIntensity.val[0];
+//            self.avgG[self.count] = avgPixelIntensity.val[1];
+//            self.avgB[self.count++] = avgPixelIntensity.val[2];
+//        } else {
+//            NSLog(@"Pixel Array is Full!");
+//            self.count = 0;
+//        }
+//
+//    }
+//    self.image = image;
+//}
+
 -(void)processImage{
     
     cv::Mat image_copy;
-    char text[35];
     Scalar avgPixelIntensity;
     cv::Mat image = self.image;
     cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
     avgPixelIntensity = cv::mean( image_copy );
-    sprintf(text,"Avg. R: %.0f, G: %.0f, B: %.0f", avgPixelIntensity.val[0],avgPixelIntensity.val[1],avgPixelIntensity.val[2]);
-    cv::putText(image, text, cv::Point(50, 50), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+    NSLog(@"%f", avgPixelIntensity[0]);
     //Set threshhold to detect finger
-    char text2[16] = "Finger Detected";
-    if(avgPixelIntensity.val[0]>235 && avgPixelIntensity.val[1]<45 && avgPixelIntensity.val[2]<45){
-        cv::putText(image, text2, cv::Point(50, 200), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
-        if (self.count < 100){
-            self.avgR[self.count] = avgPixelIntensity.val[0];
-            self.avgG[self.count] = avgPixelIntensity.val[1];
-            self.avgB[self.count++] = avgPixelIntensity.val[2];
+    if(avgPixelIntensity.val[0]>230 && avgPixelIntensity.val[1]<50 && avgPixelIntensity.val[2]<50){
+        //cv::putText(image, text2, cv::Point(50, 200), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+        if (self.count < 150){
+            self.avgR[self.count++] = avgPixelIntensity.val[0];
         } else {
-            NSLog(@"Pixel Array is Full!");
+            //do peak finding and calculating BPM here
+            float bpm = [self calculateBPM:self.avgR];
+            sprintf(self.text,"BPM: %.0f", bpm);
             self.count = 0;
         }
-        
+    }
+    cv::putText(image, self.text, cv::Point(50, 50), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+    self.image = image;
+}
+             
+             
+-(int) calculateBPM:(float*)magBuffer {
+    NSMutableArray* peaks = [[NSMutableArray alloc] init];
+    for(int i = 0;i<150;++i){
+        if([self isPeak:magBuffer index:(int)i]){
+            [peaks addObject:[NSNumber numberWithInt:i]];
+        }
+    }
+    //now we have a list of the maximum peaks
+    //get average distance (in indices) between peaks
+    int sumDistances = 0;
+    for(int i=1;i<[peaks count];++i){
+        sumDistances+= [[peaks objectAtIndex:i] intValue] - [[peaks objectAtIndex:i-1] intValue];
+    }
+    float avgDistance = sumDistances/(float)[peaks count];
+    //fps*60sec/avgdist
+    return 20*60/avgDistance;
+}
+
+-(bool) isPeak:(float*)magBuffer index:(int)index{
+    int min = 0;
+    int max = 149;
+    int window = 10;
+    
+    float threshhold = 245.0;
+    
+    if(index-window>min){
+        min = index-window;
+    }
+    if(index+window<max){
+        max = index+window;
     }
     
+    int maxIndex = min;
+    float maxValue = magBuffer[maxIndex];
     
-    
-    self.image = image;
+    for(int x = min;x<=max;++x){
+        if(magBuffer[x]>=maxValue && magBuffer[x]>threshhold){
+            maxIndex = x;
+            maxValue = magBuffer[x];
+        }
+    }
+    return (maxIndex==index);
     
 }
+
+
 
 //- (NSArray *)butterworthBandpassFilter:(float *)inputData
 //{
